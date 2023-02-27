6ac53ae3d77e2fdb29532c2121d4b333582e6872
Aleksandr Vasin <zended@yandex.ru>
2023-02-27 16:55:05 +0300

    fix: format JSON

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { JsonViewer } from '../../src';
+import json from './json.json'
 
 function App() {
   return (
-    <JsonViewer text="test" />
+    <JsonViewer json={json}/>
   );
 }
 
